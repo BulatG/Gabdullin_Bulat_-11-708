5134c46a1560c23763ecf54d3cb103afed20cf2d
BulatG <31705231+BulatG@users.noreply.github.com>
2017-09-11 21:00:50 +0300

    Add files via upload

---
+++ 2017/FALL2017/PS/SEM/PR/MainMS/MainMS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            b = b - a;
+            a = b + a;
+            b = a - b;
+            Console.WriteLine((a.ToString())+  ' ' +(b.ToString()));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2017/FALL2017/PS/SEM/PR/s.2/s.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace s._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            int b = (a / 100);
+            a%= 100;
+            b+= (a % 10) * 10;
+            b+= (a % 10) * 100;
+            Console.WriteLine(b);
+            Console.ReadLine();
+
+        }
+    }
+}
